154fc5df936e18ec2926759d15d8b81ea5a8a7ae
Dylan Butler <dylanbutler@outlook.com.au>
2024-03-26 11:15:13 +0900

    fix imager

--- go/imager/imager.go
+++ go/imager/imager.go
@@ -5,35 +5,46 @@ import (
 	"fmt"
 	"image"
 	"image/jpeg"
+	"io"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 
+	jis "github.com/dsoprea/go-jpeg-image-structure/v2"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/nfnt/resize"
 )
 
+const (
+	markdownRoot   = "site/content"
+	imageStorePath = "site/content/images"
+)
+
+var (
+	// Find image in markdown file
+	rMarkdownImage = regexp.MustCompile("!\\[[^]]*]\\(([^)]*)\\)")
+
+	// Find filename in url
+	rFilename = regexp.MustCompile("[^/]*$")
+)
+
+const imgtemplate = `{{< lazyimage %s >}}`
+
 func main() {
-	files, err := flatFiles("site/content")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	rImage, err := regexp.Compile("!\\[.*]\\((.*)\\)")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	rFilename, err := regexp.Compile("[^/]*$")
+	files, err := flatFiles(markdownRoot)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	eg := &errgroup.Group{}
-
 	for _, file := range files {
+		if !strings.Contains(file, ".md") {
+			continue
+		}
+
 		fileBytes, err := os.ReadFile(file)
 		if err != nil {
 			panic(err.Error())
@@ -41,7 +52,7 @@ func main() {
 
 		var images []string
 
-		fImages := rImage.FindAllSubmatch(fileBytes, -1)
+		fImages := rMarkdownImage.FindAllSubmatch(fileBytes, -1)
 		for _, fImage := range fImages {
 			images = append(images, string(fImage[1]))
 		}
@@ -56,7 +67,7 @@ func main() {
 			eg.Go(func() error {
 				defer fWait.Done()
 
-				err = cacheimage(image, "site/content/static/"+local)
+				err = resizeAndStore(image, imageStorePath+"/"+local, []int{240, 480, 960, 0})
 				if err != nil {
 					fmt.Println(err.Error())
 					return nil
@@ -82,34 +93,76 @@ func main() {
 	}
 }
 
-const imgtemplate = `{{< lazyimage %s >}}`
-
-func cacheimage(from, local string) error {
-	response, err := http.Get(from)
+// resizeAndStore downloads an image from a url and stores a resized version for
+// each of the widths defined. A width of 0 will keep the original width.
+func resizeAndStore(url, filenameBase string, widths []int) error {
+	// Fetch image
+	response, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer response.Body.Close()
 
-	image, _, err := image.Decode(response.Body)
+	exifReader := &bytes.Buffer{}
+	imageReader := io.TeeReader(response.Body, exifReader)
+
+	// Load image data
+	img, _, err := image.Decode(imageReader)
 	if err != nil {
 		return err
 	}
-	s := sizes(image)
 
-	for i, size := range s {
-		resized := resize.Resize(size[0], size[1], image, resize.Lanczos3)
+	// Load exif data
+	mc, err := jis.NewJpegMediaParser().ParseBytes(exifReader.Bytes())
+	if err != nil {
+		return err
+	}
+	sl := mc.(*jis.SegmentList)
 
-		err = os.MkdirAll("site/content/static", os.ModePerm)
+	// Ensure images directory exists
+	err = os.MkdirAll(imageStorePath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	for i, width := range widths {
+		x1 := img.Bounds().Size().X
+		y1 := img.Bounds().Size().Y
+
+		if width == 0 {
+			x1 = width
+		}
+
+		x2 := width
+		y2 := newY(x1, y1, x2)
+
+		// Resize image
+		resized := resize.Resize(uint(x2), uint(y2), img, resize.Lanczos3)
+
+		// Create file for size
+		suffix := ""
+		if width > 0 {
+			suffix = fmt.Sprintf("_%d", i)
+		}
+		file, err := os.Create(filenameBase + suffix)
 		if err != nil {
 			return err
 		}
-		file, err := os.Create(local + []string{"_0", "_1", "_2", ""}[i] + ".jpeg")
-		if err != nil {
-			return err
-		}
 
+		// Write image data to file
 		err = jpeg.Encode(file, resized, nil)
+		if err != nil {
+			return err
+		}
+
+		// Reset write pointer
+		_, err = file.Seek(0, 0)
+		if err != nil {
+			return err
+		}
+
+		// Write exif data to file
+		err = sl.Write(file)
 		if err != nil {
 			return err
 		}
@@ -118,25 +171,13 @@ func cacheimage(from, local string) error {
 	return nil
 }
 
-func sizes(img image.Image) [][2]uint {
-	var s [][2]uint
-
-	width := img.Bounds().Size().X
-	height := img.Bounds().Size().Y
-
-	aspectRatio := float64(width) / float64(height)
-
-	widths := []int{240, 480, 960, width}
-
-	for _, w := range widths {
-		y := int(float64(w) / aspectRatio)
-		s = append(s, [2]uint{uint(w), uint(y)})
-	}
-
-	return s
+// newY calculates the new height for an image, maintaining aspect ratio
+func newY(oldX, oldY, newX int) int {
+	aspectRatio := float64(oldX) / float64(oldY)
+	return int(float64(newX) / aspectRatio)
 }
 
-// return all files in directory and all subdirectories
+// flatFiles returns a list of all files inside directory and all subdirectories
 func flatFiles(name string) ([]string, error) {
 	var entries []string
 	readEntries, err := os.ReadDir(name)
