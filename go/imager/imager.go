dba3a72e1f71ead1294b80a1400f7514a7cb0bee
Dylan Butler <dylanbutler@outlook.com.au>
2024-03-31 14:14:28 +0900

    untest

--- go/imager/imager.go
+++ go/imager/imager.go
@@ -205,20 +205,14 @@ type fileLocker struct {
 }
 
 func (f *fileLocker) Lock(file string) {
-	fmt.Printf("Locking fl...\n")
 	f.mu.Lock()
-	fmt.Printf("Locked fl\n")
 	if _, ok := f.fl[file]; !ok {
 		f.fl[file] = &sync.Mutex{}
 	}
 	f.mu.Unlock()
-	fmt.Printf("Unlocked fl\n")
-	fmt.Printf("Locking %s...\n", file)
 	f.fl[file].Lock()
-	fmt.Printf("Locked %s\n", file)
 }
 
 func (f *fileLocker) Unlock(file string) {
 	f.fl[file].Unlock()
-	fmt.Printf("Unlocked %s\n", file)
 }
